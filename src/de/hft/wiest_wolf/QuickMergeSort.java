3c7bb17f6b0ccd2761b31b6453c35ff137a81126
9Lukas5 <9Lukas5@users.noreply.github.com>
2018-10-22 13:08:06 +0200

    import Skeleon

---
+++ src/de/hft/wiest_wolf/QuickMergeSort.java
@@ -0,0 +1,24 @@
+package de.hft.wiest_wolf;
+
+/**
+ *
+ * @author Lukas Wiest
+ * @author Erik Wolf
+ */
+public class QuickMergeSort
+{
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+    }
+
+    public static void quickSort(int[] input)
+    {
+        // Erik-TODO: implement quickSort
+    }
+
+    public static void mergeSort(int[] input)
+    {
+        // Lukas-TODO: implement mergeSort
+    }
+}
